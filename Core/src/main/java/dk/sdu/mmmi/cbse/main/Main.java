dbbdede99738fcf50a15b97eacc8bace31446f06
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2024-05-19 16:09:22 +0200

    +

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -43,6 +43,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage window) throws Exception {
+        Platform.setImplicitExit(true);
         Text text = new Text(10, 20, "Score: 0");
         text.setFill(Color.WHITE);
 
